07fbf5a1346deacae83c2c8ef1246aa60e9dbd1e
Dinara <dinozavrik.dina95@gmail.com>
2019-09-10 12:07:05 +0200

    base2_ex1

---
+++ exercice _1/index.js
@@ -0,0 +1,9 @@
+// document.getElementById("image1").addEventListener("mouseover",function(){
+// this.src="images/image1_2.jpg";
+
+// });
+var message = "Merci pour votre participation";
+ document.getElementById("nom").addEventListener("mouseout",onMouseOut){
+
+alert(message);
+ }
