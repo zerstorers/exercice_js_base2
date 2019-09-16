2efb85b46006a647fe56b3de0d0c493a348dd1cc
zerstorers <lemaire-maxime@hotmail.fr>
2019-09-16 11:06:09 +0200

    exo

---
+++ exercice 6/script.js
@@ -0,0 +1,10 @@
+for (let i = 1; i < 6; i++) {  
+    console.log("images/image"+i+"_2.jpg");
+      
+    document.getElementById("image"+i).addEventListener("mouseover", function(){
+        document.getElementById("image"+i).src = "images/image"+i+"_2.jpg"
+    })
+    document.getElementById("image"+i).addEventListener("mouseleave", function(){
+        document.getElementById("image"+i).src = "images/image"+i+".jpg"
+    })
+}
