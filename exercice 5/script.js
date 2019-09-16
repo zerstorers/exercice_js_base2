181d1e8582e27ef96de3c80d19a3dae9badd5281
zerstorers <lemaire-maxime@hotmail.fr>
2019-09-16 10:55:46 +0200

    exo

--- exercice 5/script.js
+++ exercice 5/script.js
@@ -1,33 +1,8 @@
-// var imgall = document.getElementById("image1") + document.getElementById("image2") + document.getElementById("image3") + document.getElementById("image4") + document.getElementById("image5");
-
-// imgall.addEventListener("mouseover", function(){
-//     this.src = "images/image1_2.jpg"
-
-// });
-
-
-
-var img2 = docu var img4 = document.getElementById("image4");
-var  document.getElementById("image2");
-var img3 = document.getElementById("image3");
-var img5 = document.getElementById("image5");
-
-img1, img2, img3, img4, img5.addEventListener("mouseover" , function(){
-     img1.src = "images/image1_2.jpg"
-});
-
- img2.addEventListener("mouseover" , function(){
-     this.src = "images/image2_2.jpg"
-});
-
- img3.addEventListener("mouseover" , function(){
-     this.src = "images/image3_2.jpg"
-});
-
- img4.addEventListener("mouseover" , function(){
-     this.src = "images/image4_2.jpg"
-});
-
- img5.addEventListener("mouseover" , function(){
-     this.src = "images/image5_2.jpg"
-});
+for (let i = 1; i < 6; i++) {    
+     document.getElementById("image"+i).addEventListener("mouseover", function(){
+     document.getElementById("image"+i).src = "images/image"+i+"_2.jpg"
+ })
+     document.getElementById("image"+i).addEventListener("mouseleave", function(){
+     document.getElementById("image"+i).src = "images/image"+i+".jpg"
+ })
+}
